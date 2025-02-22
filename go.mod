ab85d5ef7a3035b83bd494409c54bf3ad6174684
Vortex <73261680+Vortex2Oblivion@users.noreply.github.com>
2025-02-22 14:57:08 -0500

    they are plentiful

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module boomballtest/boomball
+module boomball/boomball
 
 go 1.24.0
 
--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 
 	var boomballTexture rl.Texture2D = rl.LoadTexture("7Dogs.png")
 	rl.SetTextureFilter(boomballTexture, rl.FilterBilinear)
-	var boomball boomball = NewBoomball(0, 0, boomballTexture)
+	var boomballs []boomball = []boomball{NewBoomball(0, 0, boomballTexture)}
 
 	var windowIcon rl.Image = *rl.LoadImage("7Dogs.png")
 	rl.SetWindowIcon(windowIcon)
@@ -28,14 +28,22 @@ func main() {
 
 		rl.ClearBackground(color.RGBA{})
 
-		boomball.draw()
-		boomball.x = float32(math.Tan(rl.GetTime()*1.5))*(1280-float32(boomballTexture.Width)*3) + float32(rl.GetScreenWidth())/2 - float32(boomballTexture.Width)/2
-		boomball.y = float32(math.Sin(rl.GetTime()*1.5))*(720-float32(boomballTexture.Height)*3) + float32(rl.GetScreenHeight())/2 - float32(boomballTexture.Height)/2
-		boomball.angle = float32(math.Tan(rl.GetTime()*1.5)) * 45
-		boomball.color = rl.ColorFromHSV(float32(rl.GetTime())*15, 1, 1)
+		for i := range len(boomballs) {
+			var f = float64(i)
+			boomballs[i].draw()
+			boomballs[i].x = float32(math.Tan(f*500+rl.GetTime()*1.5))*(1280-float32(boomballTexture.Width)*3) + float32(rl.GetScreenWidth())/2 - float32(boomballTexture.Width)/2
+			boomballs[i].y = float32(math.Sin(f*500+rl.GetTime()*1.5))*(720-float32(boomballTexture.Height)*3.25) + float32(rl.GetScreenHeight())/2 - float32(boomballTexture.Height)/2
+			boomballs[i].angle = float32(math.Tan(f*15+rl.GetTime()*1.5)) * 45
+			boomballs[i].color = rl.ColorFromHSV(float32(rl.GetTime()+f*15)*15, 1, 1)
 
-		if rl.IsKeyPressed(rl.KeyF1) {
-			boomball.rainbowMode = !boomball.rainbowMode
+			if rl.IsKeyPressed(rl.KeyF1) {
+				boomballs[i].rainbowMode = !boomballs[i].rainbowMode
+			}
+		}
+
+		if rl.IsKeyPressed(rl.KeyF2) {
+			boomballs = append(boomballs, NewBoomball(0, 0, boomballTexture))
+			boomballs[len(boomballs)-1].rainbowMode = boomballs[0].rainbowMode
 		}
 
 		rl.EndDrawing()
